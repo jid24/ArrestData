57d531ff9588906715340867278030d60092d819
jid24 <jid24@pitt.edu>
2017-03-24 16:41:47 -0400

    clean data

---
+++ Function.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+setwd("C:/Users/dengj/Desktop/2016-2017 Spring/R/data")
+dat=read.csv("ArrestData.csv")
+y<-regexpr("/",dat$OFFENSES)
+idx<-which(y>0)
+offense<-NULL
+i=0
+for(n in 1:3100){
+  i=i+1
+  offense[i]<-substr(dat$OFFENSES[idx[n]],1,y[idx[n]]-1)
+}
+dat1<-dat[idx,]
+dat1$OFFENSES<-offense
+dat2<-dat[-idx,]
+arrest<-rbind(dat1,dat2)
+
+y6<-regexpr(" ",arrest$OFFENSES)
+
+substr(arrest$OFFENSES[7],1,y6[7])
+
+code<-NULL
+i=0
+for(n in 1:4805){
+  i=i+1
+  code[i]<-substr(arrest$OFFENSES[n],1,y6[n]-1)
+}
+
+arrest<-data.frame(arrest$AGE,arrest$GENDER,arrest$OFFENSES,code)
