dbe116f7e9aedea47dc58c86dbdf16e4e998376a
Vtechdudee <fanyacheng@gmail.com>
2017-03-29 12:48:21 -0400

    Add race plot function

--- Plot/Plot.R
+++ Plot/Plot.R
@@ -26,3 +26,18 @@ plotbyage<-function(x){
           col=brewer.pal(3,"Set2"),border="white",ylim=c(0,730),
           ylab="Number of Crime",main="Top 5 Crime among Age")
   }
+
+plotbyrace<-function(x){
+  five<-x
+  colnames(five)<-c("age","gender","offense","race","code")
+  idx1<-which(five$race=="B")
+  idx2<-which(five$race=="W")
+  idx3<-which(five$race!="B"&five$race!="W")
+  a<-tapply(five$race[idx1],five$code[idx1],length)
+  b<-tapply(five$race[idx2],five$code[idx2],length)
+  c<-tapply(five$race[idx3],five$code[idx3],length)
+  z<-data.frame(a,b,c)
+  barplot(as.matrix(t(z)),beside=T,legend.text =c("Black","White","Other"),args.legend = list(bty="n",horiz=T),
+          col=brewer.pal(3,"Paired"),border="white",ylim=c(0,550),
+          ylab="Number of Crime",main="Top 5 Crime among Race")
+}
